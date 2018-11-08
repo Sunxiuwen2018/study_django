7d8315ef845371a4d0f4b39e659029322d3d205b
Sunxiuwen2018 <xiuwensun@163.com>
2018-11-09 00:18:58 +0800

    change filename and delete old dir

--- A_online_book_mall/book_supermarket/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from book_supermarket import models
-
-# Register your models here.
-
-
-for table in models.__all__:
-    admin.site.register(getattr(models, table))
--- A_online_book_mall/book_supermarket/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookSupermarketConfig(AppConfig):
-    name = 'book_supermarket'
